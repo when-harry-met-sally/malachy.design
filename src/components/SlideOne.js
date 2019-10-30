38188e292435ba79c51a85265a4093d9b55ce6e4
Miles <miles-moran@hotmail.com>
2019-10-30 12:58:43 -0500

    test

--- src/components/SlideOne.js
+++ src/components/SlideOne.js
@@ -1,10 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const SlideOne = () => {
+  const [left, setLeft] = useState(null);
+  const [top, setTop] = useState(null);
+  useEffect(() => {
+    const resizeBackground = () => {
+      const innerFrame = document.querySelector(".inner-frame");
+      setLeft(innerFrame.getBoundingClientRect().left * -1);
+      setTop(innerFrame.getBoundingClientRect().top * -1);
+    };
+    window.addEventListener("resize", () => {
+      resizeBackground();
+    });
+    setTimeout(() => {
+      resizeBackground();
+    }, 50);
+  }, []);
   return (
     <div className="slider-frame">
-        <div className='inner-frame'>
-    {/* <div className="row false-nav-one valign-wrapper">
+      <div className="inner-frame">
+        {/* <div className="row false-nav-one valign-wrapper">
       <div className="col s6 false-logo-one">Visit Mexico</div>
       <div className="col s2"></div>
       <div className="col s4 right-align">
@@ -21,8 +36,8 @@ const SlideOne = () => {
     </div>
       <div className="false-button-one"><div className='vh-center false-button-one-content'>BOOK A TRIP</div></div>
     </div> */}
-    <div className='inner-frame'
-  </div>
+      </div>
+    </div>
   );
 };
 
